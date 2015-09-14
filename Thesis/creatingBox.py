f261abb9d3203cf762adeda6963d436fdc579d50
TimIainMarsh <timiainmarsh@gmail.com>
2015-09-14 19:47:29 +0200

    added box creation .py

---
+++ Thesis/creatingBox.py
@@ -0,0 +1,42 @@
+import random
+f = open("box.obj","w")
+maxX = maxY = maxZ = 1
+minX = minY = minZ = 0
+count = 0
+while count < 10000:
+	x = str(1.0)
+	y = str(random.randrange(minY * 100,maxY * 100)/100.0)
+	z = str(random.randrange(minZ * 100,maxZ * 100)/100.0)
+	f.write("v "+x + " " + y + " " + z +"\n")
+
+	x = str(random.randrange(minX * 100,maxX * 100)/100.0)
+	y = str(1.0)
+	z = str(random.randrange(minZ * 100,maxZ * 100)/100.0)
+	f.write("v "+x + " " + y + " " + z +"\n")
+
+	x = str(random.randrange(minX * 100,maxX * 100)/100.0)
+	y = str(random.randrange(minY * 100,maxY * 100)/100.0)
+	z = str(1.0)
+	f.write("v "+x + " " + y + " " + z +"\n")
+
+	x = str(random.randrange(minX * 100,maxX * 100)/100.0)
+	y = str(random.randrange(minY * 100,maxY * 100)/100.0)
+	z = str(0.5)
+	f.write("v "+x + " " + y + " " + z +"\n")
+
+	x = str(0.0)
+	y = str(random.randrange(minY * 100,maxY * 100)/100.0)
+	z = str(random.randrange(minZ * 100,maxZ * 100)/100.0)
+	f.write("v "+x + " " + y + " " + z +"\n")
+
+	x = str(random.randrange(minX * 100,maxX * 100)/100.0)
+	y = str(0.0)
+	z = str(random.randrange(minZ * 100,maxZ * 100)/100.0)
+	f.write("v "+x + " " + y + " " + z +"\n")
+
+	x = str(random.randrange(minX * 100,maxX * 100)/100.0)
+	y = str(random.randrange(minY * 100,maxY * 100)/100.0)
+	z = str(0.0)
+	f.write("v "+x + " " + y + " " + z +"\n")
+
+	count += 1
